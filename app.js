61adbf7e497a9c0de1719f1c15b09a47cf7558de
Don Yu <don@betterment.com>
2013-11-09 14:52:30 -0500

    base express app to deploy to heroku

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var yodlee = require('./yodlee');
+var express = require('express');
+var http = require('http');
+var app = express();
 
 
-yodlee.login('sbMemadamlangsner1', 'sbMemadamlangsner1#123', function() {
-    console.log(sessionToken);
+// yodlee.login('sbMemadamlangsner1', 'sbMemadamlangsner1#123', function() {
+//     console.log(sessionToken);
+// });
+
+app.get('/', function(req, res) {
+	res.send('test');
 });
+
+http.createServer(app).listen(4000);
