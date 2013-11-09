ef81830cbdc42d178de512e465b70ce4f8ac9fac
Adam Langsner <adamlangsner@gmail.com>
2013-11-09 12:48:47 -0500

    stubbed out http client

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
-var http = require('http');
+http.request({
+    host:'localhost',
+    port:8069,
+    path:'/tickets/'+request.query.token,
+    method:'GET'
+},
+    function(res) {
+        var data = '';
+        res.on('data', function(chunk) { data += chunk; });
+        res.on('end', function() {
 
-
+        });
+    }).end();
