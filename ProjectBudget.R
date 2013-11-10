0cc131cec15eb59a79a5b663d3b3507227060973
Adam Langsner <adamlangsner@gmail.com>
2013-11-09 23:53:38 -0500

    trying to do R

--- ProjectBudget.R
+++ ProjectBudget.R
@@ -1,61 +1,60 @@
-
-rm(list=ls())
-require(ULF, rjson)
 require(RJSONIO)
+# rm(list=ls())
+# require(ULF, rjson)
 
 #'  Function to project balance
 
-getProjection <- function (jsonObj) {
-    stmt <-  fromJSON(file="~/Documents/BudgetProjector/data/data.json")
-    ob1<- as.data.frame(t(as.matrix(unlist(stmt[1]))), stringsAsFactors=FALSE)
-    ob2<- as.data.frame(t(as.matrix(unlist(stmt[2]))), stringsAsFactors=FALSE)
-    obs<- rbind(ob1, ob2)
-    for(i in 3:length(stmt)) {  #This is a horrible way to do this..
-      #print(i)
-      thisOb<- as.data.frame(t(as.matrix(unlist(stmt[i]))), stringsAsFactors=FALSE)
-      thisOb<- subset(thisOb, select=names(obs))
-      obs<-rbind(obs,thisOb)
-    }
+getOptimalPortfolio <- function (jsonObj) {
+    res <- c(1, 2, 3)
+    return(toJSON(res))
+    # stmt <-  jsonObj
+    # ob1<- as.data.frame(t(as.matrix(unlist(stmt[1]))), stringsAsFactors=FALSE)
+    # ob2<- as.data.frame(t(as.matrix(unlist(stmt[2]))), stringsAsFactors=FALSE)
+    # obs<- rbind(ob1, ob2)
+    # for(i in 3:length(stmt)) {  #This is a horrible way to do this..
+    #   #print(i)
+    #   thisOb<- as.data.frame(t(as.matrix(unlist(stmt[i]))), stringsAsFactors=FALSE)
+    #   thisOb<- subset(thisOb, select=names(obs))
+    #   obs<-rbind(obs,thisOb)
+    # }
 
-    stmt<-obs
-    stmt$amount<- as.numeric(stmt$amount)
-    currentBalance<- as.numeric(stmt$currentBalance)
-    stmt<-subset(stmt,!is.na(amount))
-    stmt$date<-as.Date(substr(stmt$postDate,start=0,stop=10), format="%Y-%m-%d")
-    stmt$amount[stmt$transactionBaseType=="debit"] <- -stmt$amount[stmt$transactionBaseType=="debit"]
-    stmt$mday<- as.POSIXlt(stmt$date)$mday
+    # stmt<-obs
+    # stmt$amount<- as.numeric(stmt$amount)
+    # currentBalance<- as.numeric(stmt$currentBalance)
+    # stmt<-subset(stmt,!is.na(amount))
+    # stmt$date<-as.Date(substr(stmt$postDate,start=0,stop=10), format="%Y-%m-%d")
+    # stmt$amount[stmt$transactionBaseType=="debit"] <- -stmt$amount[stmt$transactionBaseType=="debit"]
+    # stmt$mday<- as.POSIXlt(stmt$date)$mday
 
-    # Estimate Inflow Function ------------------------------------------------
-    stmt$amountW<- deOutlier(stmt$amount) # Removes extreme observations of cashflows.
-    # plot(amountW~amount, data=stmt)
+    # # Estimate Inflow Function ------------------------------------------------
+    # stmt$amountW<- deOutlier(stmt$amount) # Removes extreme observations of cashflows.
+    # # plot(amountW~amount, data=stmt)
 
-    # inflowItems<-subset(stmt, amountW>0)
-    # outflows<- aggregate(amountW ~ dom, data=subset(stmt, amount<0) , sum)
-    # plot(amountW ~ dom, data=outflows, las=1,type="h", main="Outflows by DOM")
-     netCashFlows<- aggregate(amountW ~ date, data=stmt , sum)
-     netCashFlows$mday<- as.POSIXlt(netCashFlows$date)$mday
+    # # inflowItems<-subset(stmt, amountW>0)
+    # # outflows<- aggregate(amountW ~ dom, data=subset(stmt, amount<0) , sum)
+    # # plot(amountW ~ dom, data=outflows, las=1,type="h", main="Outflows by DOM")
+    #  netCashFlows<- aggregate(amountW ~ date, data=stmt , sum)
+    #  netCashFlows$mday<- as.POSIXlt(netCashFlows$date)$mday
 
-    #EODBalances<- cumsum(c(currentBalance, netCashFlows$amountW))[-1]
-    # head(EODBalances)
-    # plot(EODBalances ~ netCashFlows$date, type="s")
+    # #EODBalances<- cumsum(c(currentBalance, netCashFlows$amountW))[-1]
+    # # head(EODBalances)
+    # # plot(EODBalances ~ netCashFlows$date, type="s")
 
-    # Formula projecing net cashflows on a given day.
-    netCashFlows$mday<-as.factor(netCashFlows$mday)
-    lm1<-lm(amountW ~ mday, data=netCashFlows )
+    # # Formula projecing net cashflows on a given day.
+    # netCashFlows$mday<-as.factor(netCashFlows$mday)
+    # lm1<-lm(amountW ~ mday, data=netCashFlows )
 
-    #summary(lm1)
-    #table(netCashFlows$mday)
+    # #summary(lm1)
+    # #table(netCashFlows$mday)
 
-    output<- data.frame(projectedCashFlows=predict(lm1, newdata=data.frame(mday=rep(levels(as.factor(netCashFlows$mday)),12))))
+    # output<- data.frame(projectedCashFlows=predict(lm1, newdata=data.frame(mday=rep(levels(as.factor(netCashFlows$mday)),12))))
 
-    output$projectedBalances<- cumsum(c(currentBalance[1], output$projectedCashFlows))[-1]
-    output$date <-
-      rep(sort(as.numeric(as.character(levels(netCashFlows$mday)))),12) +
-      rep(seq(0,11*30,30),each=nlevels(netCashFlows$mday))
+    # output$projectedBalances<- cumsum(c(currentBalance[1], output$projectedCashFlows))[-1]
+    # output$date <-
+    #   rep(sort(as.numeric(as.character(levels(netCashFlows$mday)))),12) +
+    #   rep(seq(0,11*30,30),each=nlevels(netCashFlows$mday))
 
-    # head(output)
-    # plot(projectedBalances~date,data=output,type="h", las=1,col="dark green")
-    return(toJSON(subset(output,select=c(date,projectedCashFlows, projectedBalances))))
+    # # head(output)
+    # # plot(projectedBalances~date,data=output,type="h", las=1,col="dark green")
+    # return(toJSON(subset(output,select=c(date,projectedCashFlows, projectedBalances))))
 }
-
-res <- getOptimalPortfolio(args)
--- ex2.R
+++ ex2.R
@@ -2,13 +2,5 @@ require(RJSONIO)
 
 getOptimalPortfolio <- function (jsonObj) {
     res <- c(1, 2, 3)
-
     return(toJSON(res))
 }
-
-# args <- '{"prods":["IBM","YHOO","MSFT"]}';
-res <- getOptimalPortfolio(args)
-
-# fromJSON(res)$pw
-
-# Optimal weights: 0.27107,0.2688,0.46013
