40dd3c51fb67256ddbd4c3a8b09b90c2a6aaef68
Adam Langsner <adamlangsner@gmail.com>
2013-11-10 03:31:28 -0500

    added logout button style touch up

--- public/assets/scripts/views/headerView.js
+++ public/assets/scripts/views/headerView.js
@@ -9,11 +9,19 @@ function($, _, Marionette, Formatter) {
     return Marionette.Layout.extend({
         template: 'header',
 
+        events: {
+            'click .logout': 'logout'
+        },
+
         initialize: function() {
             this.templateHelpers = {
                 self: this,
                 money: Formatter.money
             };
+        },
+
+        logout: function(e) {
+            location.href = '/';
         }
     });
 });
