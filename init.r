140d7c09ba271786ba3b6c172abb05c79ca2687e
Don Yu <don@betterment.com>
2013-11-09 19:35:03 -0500

    install.package should be install.packages

--- init.r
+++ init.r
@@ -1,7 +1,7 @@
 install.packages("Rserve")
 install.packages("devtools")
 install.packages("rjson")
-install.package("RJSONIO")
+install.packages("RJSONIO")
 library(devtools)
 install_github("ULF","bestdan")
 
